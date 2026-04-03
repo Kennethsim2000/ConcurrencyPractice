2534ae4a5450a1741cc20edd568446da410051a4
kenneth <kenneth12345645@gmail.com>
2026-04-03 16:45:35 +0800

    Improve on code

--- Jira.cpp
+++ Jira.cpp
@@ -15,6 +15,7 @@ enum class Priority
 
 using UserId = std::uint32_t;
 using TicketId = std::uint32_t;
+using StoryPoint = std::uint8_t;
 
 class User // a user can belong to multiple workspaces
 {
@@ -44,6 +45,7 @@ struct Ticket
     TicketId ticketId_;
     std::string description_;
     Priority priority_;
+    StoryPoint points_;
     std::chrono::system_clock::time_point deadline_;
 };
 
@@ -52,12 +54,12 @@ class WorkSpace // does not destructor due to RAII for std::vector
 public:
     WorkSpace() = default;
 
-    void addUser(const User &user)
+    void addUser(User user)
     {
         auto [it, inserted] = userIdToTicketsMap_.emplace(user.getUserId(), std::vector<uint32_t>{});
         if (inserted)
         {
-            users_.emplace_back(user);
+            users_.emplace_back(std::move(user));
         }
     }
 
@@ -71,11 +73,17 @@ public:
 
     void addTicket(const Ticket &ticket)
     {
-        tickets_.emplace_back(ticket);
+        ticketMap_[ticket.priority_].emplace_back(ticket);
     }
 
-    void assignTicket(std::uint32_t userId, std::uint32_t ticketId)
+    void assignTicket(const User &user, const Ticket &ticket)
     {
+        auto it = userIdToTicketsMap_.find(user.getUserId());
+        if (it == userIdToTicketsMap_.end())
+        {
+            throw std::invalid_argument("UserId is not a valid user");
+        }
+        it->second.push_back(ticket.ticketId_);
     }
 
     const std::vector<User> &getUsers() const
@@ -83,14 +91,15 @@ public:
         return users_;
     }
 
-    std::vector<Ticket> getTickets(Priority priority)
+    const std::vector<Ticket> &getTickets(Priority priority)
     {
+        return ticketMap_[priority];
     }
 
 private:
     std::vector<User> users_;
-    std::vector<Ticket> tickets_;
-    std::unordered_map<uint32_t, std::vector<uint32_t>> userIdToTicketsMap_;
+    std::unordered_map<Priority, std::vector<Ticket>> ticketMap_;
+    std::unordered_map<UserId, std::vector<TicketId>> userIdToTicketsMap_;
 };
 
 class Jira
@@ -114,12 +123,16 @@ int main()
     workspace.addUser(user1);
     workspace.addUser(user2);
 
-    std::vector<User> users = workspace.getUsers();
+    const auto &users = workspace.getUsers();
     auto now = std::chrono::system_clock::now();
     auto deadline = now + std::chrono::hours(24 * 3);
-    Ticket tic1(nextTicketId, "Refactor code", Priority::Medium, deadline);
+    auto deadline2 = now + std::chrono::hours(24);
+    Ticket tic1{nextTicketId, "Refactor code", Priority::Medium, 2, deadline};
+    nextTicketId++;
+    Ticket tic2{nextTicketId, "Complete migration of code", Priority::High, 5, deadline};
     nextTicketId++;
     workspace.addTicket(tic1);
+    workspace.addTicket(tic2);
     for (const auto &user : users)
     {
         std::cout << user << std::endl;
@@ -132,4 +145,6 @@ g++ Jira.cpp -o output && ./output && rm output
 Creating a Jira ticketing system, Key components: Tasks, Users, Priority
 
 Features yet to support: Identify the tasks that is assigned to a user
+Sort tasks by deadline
+
 */
